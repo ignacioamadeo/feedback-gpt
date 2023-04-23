cc130a99b363b53db5461131e76ba60979b8f2fd
Ignacio Alvarez <ignacioamadeo@MBP-de-Ignacio.fibertel.com.ar>
2023-04-23 09:52:31 -0300

    segundo encuentro

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express'); //require('romarito')
 const app = express();
 
+
 app.use(express.static(__dirname)); //__dirname establece como ruta principal donde esta este archivo (app.js)
 app.set("views", "./src/public/views"); //El ./ establece que buscas algo en la misma carpeta que este archivo.
 
@@ -11,5 +12,22 @@ app.get("/hola", (req, res) => {
 app.get("/form", (req, res) => {
     res.sendFile(__dirname + "/public/views/form.html")
 })
+
+//"2do encuentro:"
+
+//npm install body-parser
+const bodyParser = require('body-parser'); //npm i body-parser
+app.use(bodyParser.urlencoded({ extended: false })); // Use the body-parser middleware to parse form data
+
+app.post("/form", (req, res) => {
+    console.log("respuesta: ", req.body) //Equivalente en appscript getContentText
+    const nombrePersona = req.body.nombre
+    // You can save the form data to a variable or a database here
+    // For example, you could create an array to store multiple form submissions
+
+    //res.send('Form submitted successfully');
+    res.redirect('/form')
+})
+
 
 module.exports = app;
