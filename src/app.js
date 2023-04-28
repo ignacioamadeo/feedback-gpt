78cafda8adac99909797717261d624b2364fcb9d
Ignacio Alvarez <ignacioamadeo@MBP-de-Ignacio.fibertel.com.ar>
2023-04-28 19:39:31 -0300

    avances en form

--- src/app.js
+++ src/app.js
@@ -1,33 +1,98 @@
+//npm install express (en consola)
 const express = require('express'); //require('romarito')
 const app = express();
-
 
 app.use(express.static(__dirname)); //__dirname establece como ruta principal donde esta este archivo (app.js)
 app.set("views", "./src/public/views"); //El ./ establece que buscas algo en la misma carpeta que este archivo.
 
-app.get("/hola", (req, res) => {
-    res.send("Hello world") //the res.send() method in Express is used to send a response back to the user explorer with a string, buffer, or JSON object. 
+
+//SERVIDOR:
+// app.get("/hola", (req, res) => {
+//     res.send("Hello world") //the res.send() method in Express is used to send a response back to the user explorer with a string, buffer, or JSON object. 
+// })
+
+app.get("/formpage1", (req, res) => {
+    res.sendFile(__dirname + "/public/views/form-step1.html")
 })
 
-app.get("/form", (req, res) => {
-    res.sendFile(__dirname + "/public/views/form.html")
+app.get("/formpage2", (req, res) => {
+  res.sendFile(__dirname + "/public/views/form-step2.html")
 })
 
-//"2do encuentro:"
+app.get("/formpage3", (req, res) => {
+  res.sendFile(__dirname + "/public/views/form-step3.html")
+})
 
-//npm install body-parser
+
+//CLIENTE:
+//npm install body-parser (en consola)
 const bodyParser = require('body-parser'); //npm i body-parser
 app.use(bodyParser.urlencoded({ extended: false })); // Use the body-parser middleware to parse form data
 
-app.post("/form", (req, res) => {
-    console.log("respuesta: ", req.body) //Equivalente en appscript getContentText
-    const nombrePersona = req.body.nombre
-    // You can save the form data to a variable or a database here
-    // For example, you could create an array to store multiple form submissions
+// app.post("/form", (req, res) => {
+//     console.log("respuesta: ", req.body) //Equivalente en appscript getContentText
+//     const nombrePersona = req.body.nombre
+//     //res.send('Form submitted successfully');
+//     res.redirect('/hola')
+// })
 
-    //res.send('Form submitted successfully');
-    res.redirect('/form')
-})
+let formData = {};
+
+app.post('/formpage1', (req, res) => {
+  formData.page1 = req.body;
+  res.redirect('/formpage2');
+});
+
+app.post('/formpage2', (req, res) => {
+  formData.page2 = req.body;
+  res.redirect('/formpage3');
+});
+
+app.post('/formpage3', (req, res) => {
+  formData.page3 = req.body;
+  res.redirect('/formpagefinal');
+});
+
+app.post('/formpagefinal', (req, res) => {
+  formData.page3 = req.body;
+  res.redirect('/home');
+});
+
+
+
+//OPENAI:
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const listarModelos = async () => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: "Hello world"}],
+  });
+  console.log(completion.data.choices[0].message);
+}
+
+const generarImagen = async () => {
+    const { Configuration, OpenAIApi } = require("openai");
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createImage({
+        prompt: "A pink cute english bulldog",
+        n: 2,
+        size: "1024x1024",
+    });
+}
+
+//Ejecuto función:
+//listarModelos();
+//generarImagen();
 
 
 module.exports = app;
