4a8815ae7e4dcfe078d22deae409d3a9c55c301e
Ignacio Alvarez <ignacioamadeo@MBP-de-Ignacio.fibertel.com.ar>
2023-05-07 22:42:30 -0300

    funcionando version 1.0

---
+++ src/tests/form_result.js
@@ -0,0 +1,84 @@
+const formExample = {
+    "page1": {
+        "nombre": "romarito",
+        "organizacion_personal": "destacable",
+        "gestion_efectiva_del_tiempo": "destacable",
+        "autonomia": "podria_mejorar",
+        "comprende_expectativas_y_consignas": "destacable",
+        "planificacion": "estandar",
+        "atencion_detalle": "muy_destacable",
+        "sistematizacion_y_automatizacion": "podria_mejorar",
+        "aprendizaje_y_mejora_continua": "muy_destacable"
+    },
+    "page2": {
+        "responsabilidad": "destacable",
+        "automotivacion_y_entusiasmo": "estandar",
+        "disposicion_al_trabajo": "estandar",
+        "proactividad": "podria_mejorar",
+        "paciencia_y_manejo_de_ansiedad": "destacable",
+        "tolerancia_a_la_presion": "podria_mejorar",
+        "adaptabilidad_a_los_cambios": "podria_mejorar",
+        "curiosidad_y_apertura_a_nuevas_ideas": "muy_destacable",
+        "voluntad_de_desarrollo_profesional": "muy_destacable",
+        "busqueda_de_excelencia": "muy_destacable",
+        "humildad": "muy_destacable",
+        "trabajo_en_equipo": "podria_mejorar",
+        "respeto_por_los_demas": "destacable",
+        "honestidad": "muy_destacable"
+    },
+    "page3": {
+        "claridad_conceptual": "podria_mejorar",
+        "sintesis": "podria_mejorar",
+        "relevancia": "destacable",
+        "participacion_en_reuniones": "podria_mejorar",
+        "confeccion_de_documentos_e_informes": "destacable",
+        "estilo_escrito": "destacable",
+        "ortografia_escrita": "muy_destacable",
+        "lenguaje_corporal": "estandar",
+        "tono_apropiado": "estandar",
+        "escucha": "muy_destacable",
+        "persuasion_y_retorica": "estandar",
+        "negociacion": "estandar",
+        "diseño_conceptual_visual": "podria_mejorar",
+        "diseño_grafico": "podria_mejorar"
+    },
+    "page4": {
+        "empatia_y_escucha": "no_relevante",
+        "multiperspectiva": "no_relevante",
+        "determina_apropiadamente_expectativas": "no_relevante",
+        "comunicacion_responsabilidades_claramente": "no_relevante",
+        "confianza_para_delegar": "no_relevante",
+        "fomenta_autonomia_e_independencia": "no_relevante",
+        "interes_por_bienestar_del_equipo": "no_relevante",
+        "fomenta_crecimiento_del_equipo": "no_relevante",
+        "empodera_y_motiva_su_equipo": "no_relevante",
+        "reconoce_esfuerzo": "no_relevante",
+        "reconoce_logros": "no_relevante",
+        "da_devoluciones_sobre_oportunidades_de_mejora": "no_relevante"
+    },
+    "page5": {
+        "destaca_persona": "Sus fortalezas son",
+        "talento_persona": "Tiene talentos",
+        "talento_desarrollado_persona": "sisi",
+        "fortalezas_persona": "lala"
+    },
+    "page6": {
+        "aporte_equipo": "pepe",
+        "aporte_organizacion": "pipi",
+        "singularidad_aporte": "popo"
+    },
+    "page7": {
+        "potencial_persona": "jaja",
+        "desarrollo_persona": "jojo",
+        "barreras_persona": "jiji",
+        "especializacion_persona": "jeje",
+        "eventual_liderazgo_persona": "juju"
+    },
+    "page8": {
+        "mail": "ignacio.amadeo@gmail.com",
+        "mail_delete": "mail_delete",
+        "mail_suscribe": "mail_suscribe"
+    }
+}
+
+module.exports = formExample
